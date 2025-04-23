5d6598ffda042eb9633b21764bc1fcfa4cce8c16
gustavderdrache <alex.ford@determinate.systems>
2025-04-23 14:52:21 -0400

    Pin CI tools with a flake
    Also includes a .prettierrc, since later versions default to a single quote, which isn't the repo's style.

---
+++ flake.nix
@@ -0,0 +1,44 @@
+{
+  inputs = {
+    nixpkgs.url = "https://flakehub.com/f/DeterminateSystems/nixpkgs-weekly/*";
+
+    # For action-validator, which is broken with new rust versions
+    nixpkgs-old.url = "https://flakehub.com/f/NixOS/nixpkgs/0.2411.717196";
+  };
+
+  outputs =
+    { nixpkgs, nixpkgs-old, ... }:
+    let
+      inherit (nixpkgs) lib;
+
+      systems = [
+        "aarch64-linux"
+        "aarch64-darwin"
+        "x86_64-linux"
+        "x86_64-darwin"
+      ];
+
+      forEachSystem =
+        f:
+        lib.genAttrs systems (
+          system:
+          let
+            pkgs = nixpkgs.legacyPackages.${system};
+            pkgs-old = nixpkgs-old.legacyPackages.${system};
+          in
+          f { inherit pkgs pkgs-old; }
+        );
+    in
+    {
+
+      devShells = forEachSystem ({ pkgs, pkgs-old }: {
+        default = pkgs.mkShellNoCC {
+          buildInputs = [
+            pkgs.nodePackages.prettier
+
+            pkgs-old.action-validator
+          ];
+        };
+      });
+    };
+}
