a921471c7446707bde590f95733ea2528e54cb71
Luc Perkins <lucperkins@gmail.com>
2025-12-16 14:34:29 -0300

    Add TS logic

--- dist/index.mjs
+++ dist/index.mjs
@@ -1,1 +1,36 @@
+import * as actionsCore from "@actions/core";
+import * as actionsExec from "@actions/exec";
+import { DetSysAction } from "detsys-ts";
+
+//#region src/index.ts
+const EVENT_EXECUTION_FAILURE = "execution_failure";
+var DeterminateCi = class extends DetSysAction {
+	constructor() {
+		super({
+			name: "flake-iter",
+			fetchStyle: "gh-env-style",
+			diagnosticsSuffix: "telemetry",
+			requireNix: "fail"
+		});
+	}
+	async main() {
+		const binaryPath = await this.fetchExecutable();
+		const exitCode = await actionsExec.exec(binaryPath, [], {
+			env: { ...process.env },
+			ignoreReturnCode: true
+		});
+		if (exitCode !== 0) {
+			this.recordEvent(EVENT_EXECUTION_FAILURE, { exitCode });
+			actionsCore.setFailed(`Non-zero exit code of \`${exitCode}\`.`);
+		}
+	}
+	async post() {}
+};
+function main() {
+	new DeterminateCi().execute();
+}
+main();
+
+//#endregion
 export {  };
+//# sourceMappingURL=index.mjs.map
---
+++ src/index.ts
@@ -0,0 +1,43 @@
+import * as actionsCore from "@actions/core";
+import * as actionsExec from "@actions/exec";
+import { DetSysAction } from "detsys-ts";
+
+const EVENT_EXECUTION_FAILURE = "execution_failure";
+
+class DeterminateCi extends DetSysAction {
+  constructor() {
+    super({
+      name: "flake-iter",
+      fetchStyle: "gh-env-style",
+      diagnosticsSuffix: "telemetry",
+      requireNix: "fail",
+    });
+  }
+
+  async main(): Promise<void> {
+    const binaryPath = await this.fetchExecutable();
+
+    const exitCode = await actionsExec.exec(binaryPath, [], {
+      env: {
+        ...process.env, // To get $FLAKE_ITER_RUNNER_MAP or $FLAKE_ITER_NIX_SYSTEM (depending on workflow step)
+      },
+      ignoreReturnCode: true,
+    });
+
+    if (exitCode !== 0) {
+      this.recordEvent(EVENT_EXECUTION_FAILURE, {
+        exitCode,
+      });
+      actionsCore.setFailed(`Non-zero exit code of \`${exitCode}\`.`);
+    }
+  }
+
+  // No post step
+  async post(): Promise<void> {}
+}
+
+function main(): void {
+  new DeterminateCi().execute();
+}
+
+main();
