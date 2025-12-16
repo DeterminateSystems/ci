9efdf4635bff076602e00bf64f273416fa1a02f3
Luc Perkins <lucperkins@gmail.com>
2025-12-16 14:52:36 -0300

    Use tsup instead of tsdown

--- dist/index.js
+++ dist/index.js
@@ -1,42 +1,37 @@
+// src/index.ts
 import * as actionsCore from "@actions/core";
 import * as actionsExec from "@actions/exec";
 import { DetSysAction } from "detsys-ts";
-
-const EVENT_EXECUTION_FAILURE = "execution_failure";
-
-class DeterminateCi extends DetSysAction {
+var EVENT_EXECUTION_FAILURE = "execution_failure";
+var DeterminateCi = class extends DetSysAction {
   constructor() {
     super({
       name: "flake-iter",
       fetchStyle: "gh-env-style",
       diagnosticsSuffix: "telemetry",
-      requireNix: "fail",
+      requireNix: "fail"
     });
   }
-
-  async main(): Promise<void> {
+  async main() {
     const binaryPath = await this.fetchExecutable();
-
     const exitCode = await actionsExec.exec(binaryPath, [], {
       // To get $FLAKE_ITER_RUNNER_MAP or $FLAKE_ITER_NIX_SYSTEM (depending on workflow step)
-      env: process.env as Record<string, string>,
-      ignoreReturnCode: true,
+      env: process.env,
+      ignoreReturnCode: true
     });
-
     if (exitCode !== 0) {
       this.recordEvent(EVENT_EXECUTION_FAILURE, {
-        exitCode,
+        exitCode
       });
       actionsCore.setFailed(`Non-zero exit code of \`${exitCode}\`.`);
     }
   }
-
   // No post step
-  async post(): Promise<void> {}
-}
-
-function main(): void {
+  async post() {
+  }
+};
+function main() {
   new DeterminateCi().execute();
 }
-
 main();
+//# sourceMappingURL=index.js.map
