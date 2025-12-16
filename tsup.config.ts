99c6ebdf74aebb5fc9a4e8d01822a4b743beaa1a
Luc Perkins <lucperkins@gmail.com>
2025-12-16 14:59:57 -0300

    Try .cjs format

--- tsup.config.ts
+++ tsup.config.ts
@@ -1,17 +1,17 @@
-import { defineConfig } from "tsup";
 import { name } from "./package.json";
+import { defineConfig } from "tsup";
 
 export default defineConfig({
-	name,
-	entry: ["src/index.ts"],
-	format: ["esm"],
-	target: "node20",
-	bundle: true,
-	splitting: false,
-	sourcemap: true,
-	clean: true,
-	noExternal: [/.*/],
-	dts: {
-		resolve: true,
-	},
+  name,
+  entry: ["src/index.ts"],
+  format: ["cjs"],
+  target: "node20",
+  bundle: true,
+  splitting: false,
+  sourcemap: true,
+  clean: true,
+  noExternal: [/.*/],
+  dts: {
+    resolve: true,
+  },
 });
