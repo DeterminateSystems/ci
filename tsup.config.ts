678a10492f37c45501bc690c9e0f09973ed3848c
Luc Perkins <lucperkins@gmail.com>
2025-12-16 14:57:13 -0300

    Inline JS dependencies

--- tsup.config.ts
+++ tsup.config.ts
@@ -1,16 +1,17 @@
-import { name } from "./package.json";
 import { defineConfig } from "tsup";
+import { name } from "./package.json";
 
 export default defineConfig({
-  name,
-  entry: ["src/index.ts"],
-  format: ["esm"],
-  target: "node20",
-  bundle: true,
-  splitting: false,
-  sourcemap: true,
-  clean: true,
-  dts: {
-    resolve: true,
-  },
+	name,
+	entry: ["src/index.ts"],
+	format: ["esm"],
+	target: "node20",
+	bundle: true,
+	splitting: false,
+	sourcemap: true,
+	clean: true,
+	noExternal: [/.*/],
+	dts: {
+		resolve: true,
+	},
 });
